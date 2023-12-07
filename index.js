b1a2b00010411a810fb13be9814016bfbe3e1f51
pietrafreitas07 <137405907+pietrafreitas07@users.noreply.github.com>
2023-12-07 08:45:27 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,109 @@
+document.getElementById('Div1').addEventListener('click', function () {
+    let minhaDiv = document.getElementById('drown');
+
+    if (minhaDiv.style.display === 'none') {
+        minhaDiv.style.display = 'block';
+    }
+    else {
+        minhaDiv.style.display = 'none';
+    }
+});
+
+document.getElementById('Div2').addEventListener('click', function () {
+    let minhaDiv = document.getElementById('earthquake');
+
+    if (minhaDiv.style.display === 'none') {
+        minhaDiv.style.display = 'block';
+    }
+    else {
+        minhaDiv.style.display = 'none';
+    }
+});
+
+document.getElementById('Div3').addEventListener('click', function () {
+    let minhaDiv = document.getElementById('eighth');
+
+    if (minhaDiv.style.display === 'none') {
+        minhaDiv.style.display = 'block';
+    }
+    else {
+        minhaDiv.style.display = 'none';
+    }
+});
+
+document.getElementById('Div4').addEventListener('click', function () {
+    let minhaDiv = document.getElementById('eyeshadow');
+
+    if (minhaDiv.style.display === 'none') {
+        minhaDiv.style.display = 'block';
+    }
+    else {
+        minhaDiv.style.display = 'none';
+    }
+});
+
+document.getElementById('Div5').addEventListener('click', function () {
+    let minhaDiv = document.getElementById('healthy');
+
+    if (minhaDiv.style.display === 'none') {
+        minhaDiv.style.display = 'block';
+    }
+    else {
+        minhaDiv.style.display = 'none';
+    }
+});
+
+document.getElementById('Div6').addEventListener('click', function () {
+    let minhaDiv = document.getElementById('hurricane');
+
+    if (minhaDiv.style.display === 'none') {
+        minhaDiv.style.display = 'block';
+    }
+    else {
+        minhaDiv.style.display = 'none';
+    }
+});
+
+document.getElementById('Div7').addEventListener('click', function () {
+    let minhaDiv = document.getElementById('jewelry');
+
+    if (minhaDiv.style.display === 'none') {
+        minhaDiv.style.display = 'block';
+    }
+    else {
+        minhaDiv.style.display = 'none';
+    }
+});
+
+document.getElementById('Div8').addEventListener('click', function () {
+    let minhaDiv = document.getElementById('schedule');
+
+    if (minhaDiv.style.display === 'none') {
+        minhaDiv.style.display = 'block';
+    }
+    else {
+        minhaDiv.style.display = 'none';
+    }
+});
+
+document.getElementById('Div9').addEventListener('click', function () {
+    let minhaDiv = document.getElementById('shack');
+
+    if (minhaDiv.style.display === 'none') {
+        minhaDiv.style.display = 'block';
+    }
+    else {
+        minhaDiv.style.display = 'none';
+    }
+});
+
+document.getElementById('Div10').addEventListener('click', function () {
+    let minhaDiv = document.getElementById('squirrel');
+
+    if (minhaDiv.style.display === 'none') {
+        minhaDiv.style.display = 'block';
+    }
+    else {
+        minhaDiv.style.display = 'none';
+    }
+});
